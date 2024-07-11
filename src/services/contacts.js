816531f8fd3ab373e5e2972cadeaba2acc111da8
Ilya <kulak1224@gmail.com>
2024-07-11 11:09:49 +0300

    add mongodb, controllers and utils

---
+++ src/services/contacts.js
@@ -0,0 +1,7 @@
+import Contact from './schemas/ContactSchema.js';
+
+const getAll = () => Contact.find({});
+
+const getOneById = id => Contact.findById(id);
+
+export default { getAll, getOneById };
